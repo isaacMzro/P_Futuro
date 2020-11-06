eb55b97555f6a1b394305bb1976204f3b05ab25a
isaacMzro <isaacmanzanaro@gmail.com>
2020-11-06 16:24:54 +0100

    de public a private

--- src/proyectofuturo/ProyectoFuturo.java
+++ src/proyectofuturo/ProyectoFuturo.java
@@ -18,7 +18,7 @@ public class ProyectoFuturo {
         System.out.println("Estudiante " + args[0] + " : ");
 
     }
-    public static String veredicto(String valor1, String valor2) {
+    private static String veredicto(String valor1, String valor2) {
            if (valor1.equals(valor2)) {
         return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
         }
